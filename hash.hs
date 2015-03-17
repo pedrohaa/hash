a2a5bb243cb8b826362b2d036df65aea5d2fb805
Pedro Amorim <pedrohenrique.a.a@gmail.com>
2015-03-18 00:19:39 +0100

    first version

---
+++ hash.hs
@@ -0,0 +1,19 @@
+import Data.List
+
+toRow :: (Int, Int) -> [[Int]] -> [[Int]]
+toRow _ [] = []
+toRow x (m:atrix) = let a = fit (fst x) m 0 in
+                    if a >= 0 then ((take a m) ++ (take (fst x) $ repeat (snd x))
+                                  ++ (drop (a + fst x) m)) : atrix
+                    else m : (toRow x atrix)
+
+fit :: Int -> [Int] -> Int -> Int
+fit 0 _ k = 0
+fit a [] k = -1
+fit a (x:xs) k = if x == 0 then fit (a-1) xs (k+1)
+                 else if (aux >= 0) then aux + k + 1
+                      else -1
+                      where aux = fit (a+k) xs 0
+
+goList :: [(Int, Int)] -> [[Int]] -> [[Int]]
+goList l m = foldr (toRow) m (sort l)
