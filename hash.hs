979b9f85a24bb5dc0fe14dfecac52aaad0613173
Pedro Amorim <pedrohenrique.a.a@gmail.com>
2015-03-18 02:34:48 +0100

    Almost final

--- hash.hs
+++ hash.hs
@@ -17,6 +17,32 @@ fit a (x:xs) k = if fst x == 0 then fit (a-1) xs (k+1)
 
 goList :: [(Int, Int)] -> [[(Int,Int)]] -> [[(Int,Int)]]
 goList l m = foldr (toRow) m (sort l)
+             
+foo :: [[(Int, Int)]] -> Int
+foo xs = let l = map (foldr (\x y -> snd x + y) 0) xs in
+         findMin l (minimum l)
 
---createPools :: [[Int]] -> [[Int]]
---createPools [] = 0
+--Puts a new element in the pool n
+fillPool :: Int -> (Int,Int) -> [[(Int,Int)]] -> [[(Int,Int)]]
+fillPool _ _ [] = []
+fillPool n x (m:atrix) = if n == 0 then (x:m):atrix
+                          else m : fillPool (n-1) x atrix
+
+--Puts each row in the rigth format
+filterList :: [(Int,Int)] -> [(Int,Int)]
+filterList [] = []
+filterList (x:xs) = x : (filterList $ drop (fst x - 1) xs)
+
+--
+findMin :: [Int] -> Int -> Int
+findMin [] _ = 0
+findMin (x:xs) n = if x == n then 0
+                   else 1 + findMin xs n
+
+line :: [(Int,Int)] -> [[(Int,Int)]] -> [[(Int,Int)]]
+line [] l = l
+line (x:xs) l = line xs (fillPool (foo l) x l)
+
+rows :: [[(Int,Int)]] -> [[(Int,Int)]] -> [[(Int,Int)]]
+rows [] l = l
+rows (x:xs) l = rows xs $ line (filterList x) l
